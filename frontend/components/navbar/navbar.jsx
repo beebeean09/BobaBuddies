e0b61cee769f6185f8abe7a5e2794eee1719c323
beebeean09 <vivianlee@Vivians-MacBook-Air.local>
2017-03-16 19:45:50 -0700

    edits to demo login; wasnt receiving dispatch prop

--- frontend/components/navbar/navbar.jsx
+++ frontend/components/navbar/navbar.jsx
@@ -6,17 +6,25 @@ class Navbar extends React.Component {
   constructor(props) {
     super(props);
 
-    this.handleDemoLogin = this.handleDemoLogin.bind(this);
+    // this.handleDemoLogin = this.handleDemoLogin.bind(this);
   }
 
-  handleDemoLogin(e) {
-    e.preventDefault();
-    this.props.demoLogin();
-  }
+  // componentWillReceiveProps(nextProps) {
+  //   this.props.
+  // }
+
+  // handleDemoLogin(e) {
+  //   e.preventDefault();
+  //   this.props.demoLogin();
+  // }
+  // <form onSubmit={this.handleDemoLogin}>
+  //   <input className="log-button" type="submit" value="Demo Login" />
+  // </form>
 
   render() {
     let navbarButton;
 
+    // debugger;
     if (this.props.currentUser) {
       navbarButton = <button className="navbar-main-right" className="log-button" onClick={() => this.props.logout()}>Log Out</button>;
     } else {
@@ -24,13 +32,9 @@ class Navbar extends React.Component {
       <div className="navbar-main-right">
         <Link className="log-button" to="/sign-in">Sign In</Link>
         <Link className="log-button" to="/sign-up">Sign Up</Link>
-
-        <form onSubmit={this.handleDemoLogin}>
-          <input className="log-button" type="submit" value="Demo Login" />
-        </form>
+        <button className="log-button" onClick={() => this.props.demoLogin()}>Demo Login</button>
       </div>;
     }
-
     return(
       <div className="navbar-header">
         <Link to="/" className="navbar-main-left" className="boba-buddies-text">Boba Buddies</Link>
