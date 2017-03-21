04cf4ee8ea33a1fd66d1d7722c962a93f12cbddf
beebeean09 <vivianlee@Vivians-MacBook-Air.local>
2017-03-20 20:38:48 -0700

    add events seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,21 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-demo_user = User.create({ email: "bobabuddies@gmail.com", password: "bobaboba", first_name: "Buddy", host: false })
+User.create( email: "bobabuddies@gmail.com", password: "bobaboba", first_name: "Buddy", host: false )
+User.create( email: "paul@gmail.com", password: "iloveboba", first_name: "Paul", host: true )
+User.create( email: "sarah@gmail.com", password: "iloveboba", first_name: "Sarah", host: true )
 
 City.create!(name: "San Francisco", image: 'http://res.cloudinary.com/beebeean09/image/upload/v1490057537/SF_vysi0o.jpg')
-City.create!(name: "London", image: 'http://res.cloudinary.com/beebeean09/image/upload/v1490057537/london_tqwih7.jpg')
+City.create!(name: "Chicago", image: 'http://res.cloudinary.com/beebeean09/image/upload/v1490067089/Chicago_i9fiwb.jpg')
 City.create!(name: "Los Angeles", image: 'http://res.cloudinary.com/beebeean09/image/upload/v1490057534/los_angeles_cmfmup.jpg')
 City.create!(name: "Seattle", image: 'http://res.cloudinary.com/beebeean09/image/upload/v1490057536/seattle_lcuoan.jpg')
 City.create!(name: "Washington D.C.", image: 'http://res.cloudinary.com/beebeean09/image/upload/v1490057537/washington_dc_cvix4r.jpg')
 City.create!(name: "New York", image: 'http://res.cloudinary.com/beebeean09/image/upload/v1490057535/NY_ezvbug.jpg')
+
+
+Event.create!(title: "Milk Tea Boba Party", address: "135 4th St., San Francisco, CA", city_id: 1, host_id: 2)
+Event.create!(title: "What is Boba?!", address: "333 S. State St., Chicago, IL", city_id: 2, host_id: 2)
+Event.create!(title: "How much boba...is too much boba??", address: "700 Western St. Los Angeles, CA", city_id: 3, host_id: 2)
+Event.create!(title: "Alcoholic Boba Drinks", address: "606 E. Pine St., Seattle, WA", city_id: 4, host_id: 3)
+Event.create!(title: "Cup of Boba a day, keeps the doctor away~", address: "400 8th St. NW, Washington, DC ", city_id: 5, host_id: 3)
+Event.create!(title: "Matcha Boba Drinks!", address: "23 Clinton St., New York, NY", city_id: 6, host_id: 3)
